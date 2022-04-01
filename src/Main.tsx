b9d02724fad31b8daded69af079cecc6ab5f09a5
Puffinzze <puffinzze69@gmail.com>
2022-04-01 22:30:50 +0100

    Initial Commit

---
+++ src/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FancyHeading from './components/fancy-heading';
+import './media/scss/main.scss';
+
+function Main() {
+  return (
+    <div className="App">
+      <FancyHeading text='Evil Owl Studios'/>
+    </div>
+  );
+}
+
+export default Main;
